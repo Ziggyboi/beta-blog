8dc727bad23605b29389f4bc7ae836c9ef69992e
Ziggyboi <dumbleshoot@gmail.com>
2019-04-23 20:11:10 +0100

    Create articles table, model, and add validations

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  get 'pages/home', to: 'pages#home'
-  get 'pages/about', to:'pages#about'
+  get 'home', to: 'pages#home'
+  get 'about', to:'pages#about'
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
