4758bf3b65ea9b8ebac1c53b2c1f810a439e644b
Ziggyboi <dumbleshoot@gmail.com>
2019-05-17 19:23:43 +0100

    Create Homepage and add links

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  get 'home', to: 'pages#home'
+  root 'pages#home'
   get 'about', to:'pages#about'
-
+  
   resources :articles
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
