e43004bd7a0fb020c61c5b30f4e51e6b77c53497
Ziggyboi <dumbleshoot@gmail.com>
2019-05-21 05:14:04 +0100

    updating some authentication errors(sign in upon signup, saving article with right user)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,8 +15,9 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
+            session[:user_id] = user.id
             flash[:success]="Welcome to the Alpha-Blog #{@user.username}"
-            redirect_to articles_path
+            redirect_to user_path(@user)
         else
             render 'new'
         end
@@ -52,7 +53,7 @@ class UsersController < ApplicationController
     def require_same_user
         if !logged_in? || current_user != @user
             flash[:danger] = "You can only edit your own profile"
-            redirect_to root_path
+            redirect_to root_path   
         end
 
     end
